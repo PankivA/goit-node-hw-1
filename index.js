b47c8076b27f797d804dbff9b4831343e416ad66
Andrii Pankiv <pankivand@gmail.com>
2023-07-09 16:30:53 +0300

    add hw1 code

---
+++ index.js
@@ -0,0 +1,33 @@
+const contacts = require("./contacts");
+
+const argv = require('yargs').argv;
+
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+          const getList = await contacts.listContacts();
+          console.log(getList);
+      break;
+
+    case 'get':
+          const oneContact = await contacts.getContactById(contactId);
+          console.log(oneContact);
+      break;
+
+    case 'add':
+          const newContact = await contacts.addContact(name, email, phone);
+          console.log(newContact);
+      break;
+
+    case 'remove':
+          const removeContact = await contacts.removeContact(contactId);
+          console.log(removeContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
